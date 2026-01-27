80fc1549a65315804b384a49dd31531de617936f
Jaysinh Chauhan <workmailofjay@gmail.com>
2026-01-27 16:03:21 +0530

    Complemented Phases-1

--- internal/input/windows.go
+++ internal/input/windows.go
@@ -9,6 +9,11 @@ import (
 const (
 	inputMouse     = 0
 	mouseEventMove = 0x0001
+
+	mouseEventLeftDown  = 0x0002
+	mouseEventLeftUp    = 0x0004
+	mouseEventRightDown = 0x0008
+	mouseEventRightUp   = 0x0010
 )
 
 type input struct {
@@ -63,12 +68,70 @@ func (w *windowsInjector) MoveRelative(dx int32, dy int32) error {
 	return nil
 }
 
+// LeftClick performs a native left mouse click.
 func (w *windowsInjector) LeftClick() error {
-	return errors.New("LeftClick not implemented")
+	inputs := []input{
+		{
+			Type: inputMouse,
+			Mi: mouseInput{
+				DwFlags: mouseEventLeftDown,
+			},
+		},
+		{
+			Type: inputMouse,
+			Mi: mouseInput{
+				DwFlags: mouseEventLeftUp,
+			},
+		},
+	}
+
+	ret, _, err := procSendInput.Call(
+		uintptr(len(inputs)),
+		uintptr(unsafe.Pointer(&inputs[0])),
+		unsafe.Sizeof(inputs[0]),
+	)
+
+	if ret == 0 {
+		if err != nil && err != syscall.Errno(0) {
+			return err
+		}
+		return errors.New("SendInput failed for LeftClick")
+	}
+
+	return nil
 }
 
+// RightClick performs a native right mouse click.
 func (w *windowsInjector) RightClick() error {
-	return errors.New("RightClick not implemented")
+	inputs := []input{
+		{
+			Type: inputMouse,
+			Mi: mouseInput{
+				DwFlags: mouseEventRightDown,
+			},
+		},
+		{
+			Type: inputMouse,
+			Mi: mouseInput{
+				DwFlags: mouseEventRightUp,
+			},
+		},
+	}
+
+	ret, _, err := procSendInput.Call(
+		uintptr(len(inputs)),
+		uintptr(unsafe.Pointer(&inputs[0])),
+		unsafe.Sizeof(inputs[0]),
+	)
+
+	if ret == 0 {
+		if err != nil && err != syscall.Errno(0) {
+			return err
+		}
+		return errors.New("SendInput failed for RightClick")
+	}
+
+	return nil
 }
 
 func (w *windowsInjector) Shutdown() error {
